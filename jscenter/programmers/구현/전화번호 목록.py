88fb77ba7e6c67fcedbd510ba578c065a4c441bc
cokemania2 <38974863+cokemania2@users.noreply.github.com>
2020-07-12 15:59:25 +0900

    수정

---
+++ jscenter/programmers/구현/전화번호 목록.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    answer = True
+
+    for i in range (len(phone_book)) :
+         for j in range (len(phone_book)) :
+              if (i==j) :
+                  continue
+              elif ( phone_book[j].find(phone_book[i]) == 0) :
+                  answer = False;
+                  break
+         if ( answer == False ) :
+             break
+        
+                  
+    return answer
