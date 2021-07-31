6443ba2ea85c39c61b573c5c7fc208b7e897ed46
coke <coke@cokes-MacBook-Air.local>
2021-07-31 16:20:06 +0900

    update

---
+++ codingTest/leetcode/maximum-subarray.go
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-subarray/
+// Maximum Subarray.py
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        dp = [nums[0]]
+        for i in range(1, len(nums)) :
+            if dp[i-1] > 0 :
+                dp.append(nums[i] + dp[i-1])
+            else :
+                dp.append(nums[i])
+        return max(dp)
