d37000e5d27b4b6c46ec2e0893f269cdc231233a
coke <coke@cokes-MacBook-Air.local>
2021-09-21 22:32:10 +0900

    update

---
+++ codingTest/leetcode/binary-search.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/binary-search/submissions/
+# binary-search.py
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        mid = len(nums) // 2
+        while(l <= r):
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+            mid = l + (r - l) // 2
+        return -1
