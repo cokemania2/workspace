a219d1fd4d423d647fcb576370d2ba077b05496f
coke <coke@cokes-MacBook-Air.local>
2021-10-04 15:09:42 +0900

    update

---
+++ codingTest/leetcode/rotate-array.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/rotate-array
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k =  k % len(nums)
+        nums[:] = nums[len(nums)-k:] + nums[:len(nums)-k]
