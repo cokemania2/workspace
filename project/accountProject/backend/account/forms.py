39c0d8934c5eacfd93cf87a0de08af66ae399e05
cokemania2 <coke_mania@naver.com>
2022-06-18 12:20:18 +0900

    삭제

--- project/accountProject/backend/account/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class SignUpForm(forms.Form):
-    username = forms.CharField(required=True, label='username', max_length=200)
-    password = forms.CharField(required=True, label='password', max_length=200)
-    
