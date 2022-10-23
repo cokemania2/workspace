5fe409ff04d9346e1699a1921ed12f1c9a41f31d
cokemania2 <coke_mania@naver.com>
2022-10-24 00:22:11 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -4,6 +4,7 @@ def solution(X, Y):
     answer = ''.join(sorted((Counter(X) & Counter(Y)).elements(), reverse=True))
     if answer == '':
         return "-1"
+    # return str(int(asnwer)) 방법은 시간초과가 걸림
     if answer[0] == '0':
         return "0"
     return answer
