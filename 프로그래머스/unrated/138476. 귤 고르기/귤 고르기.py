3eafb302ae07433f28734fff5f35a3a185142f3d
myoungjinGo <coke_mania@naver.com>
2023-08-15 17:31:47 +0900

    [unrated] Title: 귤 고르기, Time: 28.06 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,14 +1,11 @@
+from collections import Counter
+
 def solution(k, tangerine):
-    answer = 0 
     # 서로 다른 종류의 수를 최소화
     # 딕셔너리에 담아서 정렬
-    gyul_box = dict()
-    for t in tangerine:
-        if t in gyul_box:
-            gyul_box[t] += 1
-        else:
-            gyul_box[t] = 1
-    sorted_gyul_box = sorted(gyul_box.items(),key=lambda x:x[1],reverse=True)
+    gyul_box = Counter(tangerine)
+    sorted_gyul_box = sorted(gyul_box.items(), key=lambda x:x[1], reverse=True)
+
     for i, box in enumerate(sorted_gyul_box):
         k -= box[1]
         if k <= 0:
